8a62012edbea046883a2a57ef0ea770ca0779972
DustinScharf <d.scharf.2002@gmail.com>
2021-12-12 18:22:32 +0100

    Created login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,10 +8,78 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var txt = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("NOT READY, GO BACK..."),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(16),
+        child: Center(
+          child: Card(
+            elevation: 128,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 16.0,
+                      ),
+                      const Text(
+                        "Login",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      const Text(
+                        "Enter your credentials",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 32.0,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 50.0, right: 50.0),
+                        child: TextField(
+                          onChanged: (text) {
+                            txt = text;
+                          },
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Username",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16.0,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 50.0, right: 50.0),
+                        child: TextField(
+                          onChanged: (text) {
+                            txt = text;
+                          },
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Password",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
