b9f3831614ef11ec3b3341565810f3b1fa38ba3f
DustinScharf <d.scharf.2002@gmail.com>
2021-12-12 15:51:44 +0100

    Added route_generator.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import 'package:sepsis_monitor/route_generator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "SepsisMonitor",
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: "/",
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
