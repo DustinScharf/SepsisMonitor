31c98fd58067e040668533ffae3dbb5b1b62e7b0
DustinScharf <d.scharf.2002@gmail.com>
2021-12-15 04:09:42 +0100

    Added overview menu

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,3 +1,4 @@
 class Layout {
   static double maxWidth = 320;
+  static double btnUniWidth = 250;
 }
--- lib/overview_page.dart
+++ lib/overview_page.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'layout.dart';
+
 class OverviewPage extends StatefulWidget {
   const OverviewPage({Key? key}) : super(key: key);
 
@@ -15,11 +17,94 @@ class _OverviewPageState extends State<OverviewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () {
-          _logout();
-          Navigator.of(context).pop();
-        },
-        child: const Text("Logout"));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome"),
+      ),
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.all(16),
+          elevation: 4,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(
+                width: Layout.maxWidth,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Text(
+                      "Welcome", // todo add name
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    const Text(
+                      "What's up next?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                    SizedBox(
+                      width: Layout.btnUniWidth,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("PATIENT LIST"),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4.0,
+                    ),
+                    SizedBox(
+                      width: Layout.btnUniWidth,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("ONLY ERST LIST"),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4.0,
+                    ),
+                    SizedBox(
+                      width: Layout.btnUniWidth,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("CONFIRM ERST CASES"),
+                      ),
+                    ),
+                    // const SizedBox( // todo
+                    //   height: 4.0,
+                    // ),
+                    // SizedBox(
+                    //   width: Layout.btnUniWidth,
+                    //   child: ElevatedButton(
+                    //     onPressed: () {},
+                    //     child: const Text("CONFIRM REGISTRATION"),
+                    //   ),
+                    // ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _logout();
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("LOGOUT"),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
