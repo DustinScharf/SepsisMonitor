02499520f77c2b9387b996aa6b25501ab28d4004
DustinScharf <d.scharf.2002@gmail.com>
2021-12-18 19:59:08 +0100

    Only showing patients of logged in staff

--- lib/patient_list_page.dart
+++ lib/patient_list_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:collection';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -21,6 +22,12 @@ class _PatientListPageState extends State<PatientListPage> {
       FirebaseDatabase.instance.ref("hospital/patients");
 
   Widget _buildPatientList() {
+    if (_patients.isEmpty) {
+      return const Center(
+        child: Text("Loading..."),
+      );
+    }
+
     return ListView.builder(
         padding: const EdgeInsets.all(16),
         itemCount: _patients.length * 2 - 1,
@@ -60,13 +67,38 @@ class _PatientListPageState extends State<PatientListPage> {
     return patientInfo;
   }
 
-  _putStaffToPatient(String patientId, LinkedHashMap patient) async {
+  _putPatientsForStaff(String staffId) {
+    Query staff = _staffDbRef.child(staffId);
+    staff.onValue.listen((DatabaseEvent event) {
+      LinkedHashMap staffMap = event.snapshot.value as LinkedHashMap;
+
+      Query patientsSorted = _patientsDbRef.orderByChild("phase");
+      // Subscribe to the query TODO: get only patients of a certain staff
+      patientsSorted.onValue.listen((DatabaseEvent event) {
+        setState(() {
+          _patients.clear();
+          for (var element in event.snapshot.children) {
+            if (staffMap["patients"] == null ||
+                (staffMap["patients"] as LinkedHashMap)
+                    .containsKey(element.key)) {
+              LinkedHashMap patient =
+                  element.value as LinkedHashMap<Object?, Object?>;
+              _putStaffToPatient(element.key.toString(), patient);
+              _patients.add(patient);
+            }
+          }
+        });
+      });
+    });
+  }
+
+  _putStaffToPatient(String patientId, LinkedHashMap patient) {
     Query staffSorted = _staffDbRef.orderByChild("lastName");
     staffSorted.onValue.listen((event) {
       var staffList = event.snapshot.children.where((staffData) =>
-      ((staffData.value as LinkedHashMap).containsKey("patients") &&
-          ((staffData.value as LinkedHashMap)["patients"] as LinkedHashMap)
-              .containsKey(patientId)));
+          ((staffData.value as LinkedHashMap).containsKey("patients") &&
+              ((staffData.value as LinkedHashMap)["patients"] as LinkedHashMap)
+                  .containsKey(patientId)));
       for (var staff in staffList) {
         setState(() {
           patient.putIfAbsent("staff", () => staff.value);
@@ -87,19 +119,9 @@ class _PatientListPageState extends State<PatientListPage> {
 
   @override
   void initState() {
-    Query patientsSorted = _patientsDbRef.orderByChild("phase");
-    // Subscribe to the query TODO: get only patients of a certain staff
-    patientsSorted.onValue.listen((DatabaseEvent event) {
-      setState(() {
-        _patients.clear();
-        for (var element in event.snapshot.children) {
-          LinkedHashMap patient =
-              element.value as LinkedHashMap<Object?, Object?>;
-          _putStaffToPatient(element.key.toString(), patient);
-          _patients.add(patient);
-        }
-      });
-    });
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+    uid ??= "LOADING ERROR!"; // todo better handle
+    _putPatientsForStaff(uid);
 
     super.initState();
   }
