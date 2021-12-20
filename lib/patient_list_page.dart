966314577e1591b61962f0a49a1b6121de574ed8
DustinScharf <d.scharf.2002@gmail.com>
2021-12-20 16:57:48 +0100

    Optimized imports

--- lib/patient_list_page.dart
+++ lib/patient_list_page.dart
@@ -1,9 +1,9 @@
-import 'dart:async';
 import 'dart:collection';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:sepsis_monitor/layout.dart';
 
 class PatientListPage extends StatefulWidget {
   const PatientListPage({Key? key}) : super(key: key);
