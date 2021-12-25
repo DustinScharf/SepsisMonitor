ab7d3d64c91a49c96823cf580312b820aba85fd1
DustinScharf <d.scharf.2002@gmail.com>
2021-12-25 20:32:32 +0100

    Added browser check

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -36,7 +36,7 @@ class _StartPageState extends State<StartPage> {
 
   _browserInfo() {
     List<String> supportedBrowsers = ["Chrome", "Safari", "Chromium Edge"];
-    if (/*!supportedBrowsers.contains(Browser().browser)*/true) {
+    if (!supportedBrowsers.contains(Browser().browser)) {
       // todo not works on mobile browsers
       final Browser? _browser = Browser.detectOrNull();
       return Container(
