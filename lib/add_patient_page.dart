79f7afb4f34c331f5ee6b936c6d5d3e9a059cfc3
DustinScharf <d.scharf.2002@gmail.com>
2021-12-28 02:27:16 +0100

    Removed unused import

--- lib/add_patient_page.dart
+++ lib/add_patient_page.dart
@@ -4,7 +4,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sepsis_monitor/layout.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class AddPatientPage extends StatefulWidget {
   const AddPatientPage({Key? key}) : super(key: key);
